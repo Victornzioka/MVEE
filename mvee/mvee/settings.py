1ff9d916d17fe81365652c4feb31f920ce9b5774
Victor Nzioka <47145430+Victornzioka@users.noreply.github.com>
2024-03-26 13:06:12 +0300

    Update settings.py

--- mvee/mvee/settings.py
+++ mvee/mvee/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f#a8j(=ef7$hb@2ijb_o6x!dgw_fbv(32*39rvdk+l0z%g%%w9'
+SECRET_KEY = 'key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
