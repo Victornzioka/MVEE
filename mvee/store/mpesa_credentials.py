d955514b82ede11cd76da190d4dab9291aaeec23
Victor Nzioka <47145430+Victornzioka@users.noreply.github.com>
2024-03-26 13:04:49 +0300

    Update mpesa_credentials.py
    removing the private keys

--- mvee/store/mpesa_credentials.py
+++ mvee/store/mpesa_credentials.py
@@ -4,8 +4,8 @@ from requests.auth import HTTPBasicAuth
 from datetime import datetime
 import base64
 class MpesaC2bCredential:
-    consumer_key = 'cHnkwYIgBbrxlgBoneczmIJFXVm0oHky'
-    consumer_secret = '2nHEyWSD4VjpNh2g'
+    consumer_key = 'key'
+    consumer_secret = 'key'
     api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 class MpesaAccessToken:
     r = requests.get(MpesaC2bCredential.api_URL,
@@ -15,7 +15,7 @@ class MpesaAccessToken:
 class LipanaMpesaPpassword:
     lipa_time = datetime.now().strftime('%Y%m%d%H%M%S')
     Business_short_code = "174379"
-    passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+    passkey = 'key'
     data_to_encode = Business_short_code + passkey + lipa_time
     online_password = base64.b64encode(data_to_encode.encode())
     decode_password = online_password.decode('utf-8')
